78e89b39df74eeb7ecf8f7eb88a9f96a33d1949d
Vadym Kirilkov <tav0r0tuk@gmail.com>
2025-08-31 13:53:59 +0200

    to check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field-text');
+
+for (const input of inputs) {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  if (!input.id) {
+    input.id = 'sign-in-' + input.name;
+  }
+
+  label.setAttribute('for', input.id);
+  label.textContent = input.name;
+
+  input.before(label);
+
+  const placeholderText = input.name[0].toUpperCase() + input.name.slice(1);
+
+  input.setAttribute('placeholder', placeholderText);
+}
